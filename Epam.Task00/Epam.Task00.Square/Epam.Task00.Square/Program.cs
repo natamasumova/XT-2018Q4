2176d6e9305207befe6b565c8c918dc27167072c
natamasumova <36184558+natamasumova@users.noreply.github.com>
2018-11-20 14:19:26 +0400

    Add files via upload

---
+++ Epam.Task00/Epam.Task00.Square/Epam.Task00.Square/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Epam.Task00.Square
+{
+    class Program
+    {
+        static void PrintSquare(uint n)
+        {
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("The number should be odd. Try again, please.");
+                return;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if ((i == n / 2 + 1) && (j == n / 2 + 1))
+                    {
+                        Console.Write(" ");
+                    }
+                    else Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter N: ");
+                uint N = uint.Parse(Console.ReadLine());
+                PrintSquare(N);
+            }
+        }
+    }
+}
