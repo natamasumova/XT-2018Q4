3ed946c42ea2172b503d3aaed51008dcf4882cf7
natamasumova <36184558+natamasumova@users.noreply.github.com>
2018-11-20 14:20:04 +0400

    Add files via upload

---
+++ Epam.Task00/Epam.Task00.Simple/Epam.Task00.Simple/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Epam.Task00.Sequence
+{
+    class Program
+    {
+        static bool IsSimple(uint n)
+        {
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0) return false;
+                i++;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter N: ");
+                uint N = uint.Parse(Console.ReadLine());
+                Console.WriteLine(IsSimple(N));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
