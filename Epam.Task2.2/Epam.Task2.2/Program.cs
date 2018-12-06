d96c26ae39872717e23cfa22702ffa355666cd85
Natalya_Masumova <36184558+natamasumova@users.noreply.github.com>
2018-12-06 06:30:43 -0800

    Add files via upload

--- Epam.Task2.2/Epam.Task2.2/Program.cs
+++ Epam.Task2.2/Epam.Task2.2/Program.cs
@@ -6,6 +6,29 @@ namespace Epam.Task2._2
     {
         public static void Main(string[] args)
         {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter A: ");
+                    double a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter B: ");
+                    double b = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter C: ");
+                    double c = double.Parse(Console.ReadLine());
+
+                    Triangle triangle = new Triangle(a, b, c);
+                    Console.WriteLine(triangle.ToString());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Incorrect input. Try again please");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
---
+++ Epam.Task2.2/Epam.Task2.2/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Epam.Task2._2
+{
+    public class Triangle
+    {
+        private double A { get; set; }
+        private double B { get; set; }
+        private double C { get; set; }
+
+        public Triangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new Exception("All sides should be positive. Try again, please");
+            }
+            if (a + b < c)
+            {
+                throw new Exception("The biggest side should be more than sum of another sides. Try again, please");
+            }
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double GetHeight()
+        {
+            double triangleHeight = A * A - (A * A - B * B) / (2 * C) - C / 2;
+            return triangleHeight;
+        }
+
+        public double GetPerimeter()
+        {
+            return A * B * C;
+        }
+        
+        public double GetArea()
+        {
+            return (C * GetHeight()) / 2;
+        }
+
+        public override string ToString()
+        {
+            return $"Perimeter: {GetPerimeter()}{Environment.NewLine}Area: {GetArea()}{Environment.NewLine}";
+        }
+    }
+}
