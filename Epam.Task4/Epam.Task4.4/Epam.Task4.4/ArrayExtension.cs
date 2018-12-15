1b02a7925c634cecd9ac078faf724223e135daea
Natalya_Masumova <36184558+natamasumova@users.noreply.github.com>
2018-12-15 14:12:25 +0400

    Add files via upload

---
+++ Epam.Task4/Epam.Task4.4/Epam.Task4.4/ArrayExtension.cs
@@ -0,0 +1,29 @@
+﻿namespace Epam.Task4._4
+{
+    public static class ArrayExtension
+    {
+        public static int CountElements(this int[] array)
+        {
+            int sum = 0;
+            int i = 0;
+            while (i < array.Length)
+            {
+                sum += array[i];
+                i++;
+            }
+            return sum;
+        }
+
+        public static double CountElements(this double[] array)
+        {
+            double sum = 0;
+            int i = 0;
+            while (i < array.Length)
+            {
+                sum += array[i];
+                i++;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Epam.Task4/Epam.Task4.4/Epam.Task4.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Epam.Task4._4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 4, 3, 2, 6, 3, 3, 6, 3, 6 };
+            Console.WriteLine($"The sum of array elements is {arr.CountElements()}");
+        }
+    }
+}
